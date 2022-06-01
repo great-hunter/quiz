c028edcbbc092298188d6ab0b7975166209de029
great-hunter <greathunter@mail.ru>
2022-06-01 14:32:55 +0300

    lesson 2 all

--- script/script.js
+++ script/script.js
@@ -7,22 +7,83 @@ document.addEventListener('DOMContentLoaded', function() {
   const questionTitle = document.querySelector('#question');
   const formAnswers = document.querySelector('#formAnswers');
   const burgerBtn = document.getElementById('burger');
+  const nextButton = document.querySelector('#next');
+  const prevButton = document.querySelector('#prev');
 
   const questions = [
     {
-      question: 'Какого цвета бургер вы хотите?',
-      answewrs: [
-        {
-          title: 'Стандарт',
-          url: './image/burger.png'
-        },
-        {
-          title: 'Черныйт',
-          url: './image/burgerBlack.png'
-        }
-      ]
+        question: "Какого цвета бургер?",
+        answers: [
+            {
+                title: 'Стандарт',
+                url: './image/burger.png'
+            },
+            {
+                title: 'Черный',
+                url: './image/burgerBlack.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "Из какого мяса котлета?",
+        answers: [
+            {
+                title: 'Курица',
+                url: './image/chickenMeat.png'
+            },
+            {
+                title: 'Говядина',
+                url: './image/beefMeat.png'
+            },
+            {
+                title: 'Свинина',
+                url: './image/porkMeat.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "Дополнительные ингредиенты?",
+        answers: [
+            {
+                title: 'Помидор',
+                url: './image/tomato.png'
+            },
+            {
+                title: 'Огурец',
+                url: './image/cucumber.png'
+            },
+            {
+                title: 'Салат',
+                url: './image/salad.png'
+            },
+            {
+                title: 'Лук',
+                url: './image/onion.png'
+            }
+        ],
+        type: 'checkbox'
+    },
+    {
+        question: "Добавить соус?",
+        answers: [
+            {
+                title: 'Чесночный',
+                url: './image/sauce1.png'
+            },
+            {
+                title: 'Томатный',
+                url: './image/sauce2.png'
+            },
+            {
+                title: 'Горчичный',
+                url: './image/sauce3.png'
+            }
+        ],
+        type: 'radio'
     }
-  ]
+  ];
 
   burgerBtn.style.display = 'none';
   let clientWidth = document.documentElement.clientWidth;
@@ -68,14 +129,14 @@ document.addEventListener('DOMContentLoaded', function() {
   const playTest = () => {
     let numberQuestion = 0;
     const renderAnswers = (index) => {
-      questions[index].answewrs.forEach((answer) => {
+      questions[index].answers.forEach((answer) => {
         const answerItem = document.createElement('div');
 
         answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
 
         answerItem.innerHTML = `
-          <input type="radio" id="answerItem1" name="answer" class="d-none">
-          <label for="answerItem1" class="d-flex flex-column justify-content-between">
+          <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+          <label for="${answer.title}" class="d-flex flex-column justify-content-between">
             <img class="answerImg" src="${answer.url}">
             <span>${answer.title}</span>
           </label>
@@ -88,12 +149,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const renderQuestions = (indexQuestion) => {
+      formAnswers.innerHTML = '';
+
+
       questionTitle.textContent = `${questions[indexQuestion].question}`;
 
       renderAnswers(indexQuestion);
     }
     renderQuestions(numberQuestion);
 
+    nextButton.onclick = () => {
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+    };
+    prevButton.onclick = () => {
+      numberQuestion--;
+      renderQuestions(numberQuestion);
+    };
   }
 });
 
